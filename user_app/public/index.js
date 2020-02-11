09e731db8bd8d52f1e0f1040b11f1ecf6dcf0180
jshi3 <jshi3@caltech.edu>
2020-02-11 01:23:40 -0800

    No overwriting or eventless registration

--- user_app/public/index.js
+++ user_app/public/index.js
@@ -5,18 +5,19 @@
     var form = document.getElementById("form");
     form.onsubmit = function() {
         var eventName = document.getElementById("jumpmenu").value;
-        var eventRef = eventsRef.doc(eventName).collection("attendees");
-        var email = document.getElementById("email").value;
-        eventRef.doc(email).set({
-            "name": document.getElementById("first-name").value + " " + document.getElementById("last-name").value,
-            "caltech_uid": document.getElementById("uid").value,
-            "email": email,
-            "diet": document.getElementById("diet").value
-        });
+        if (eventName !== "none") {
+            var eventRef = eventsRef.doc(eventName).collection("attendees");
+            eventRef.add({
+                "name": document.getElementById("first-name").value + " " + document.getElementById("last-name").value,
+                "caltech_uid": document.getElementById("uid").value,
+                "email": document.getElementById("email").value,
+                "diet": document.getElementById("diet").value
+            });
+        }
     };
     var jumpMenu = document.getElementById("jumpmenu");
     jumpMenu.onchange = function() {
-        if (document.form.jumpmenu.value != null) {
+        if (document.form.jumpmenu.value !== "none") {
             document.location.href = document.form.jumpmenu.options[document.form.jumpmenu.options.selectedIndex].value;
         }
     };
