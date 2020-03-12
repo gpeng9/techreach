88eb07b90e605b87af478c68ea8084021edbdf20
jshi3 <jshi3@caltech.edu>
2020-03-11 22:50:47 -0700

    Events stored by name and date

--- web_app/public/createEvent.js
+++ web_app/public/createEvent.js
@@ -3,34 +3,28 @@ var db = firebase.firestore();
 
 function submitEvent() {
     alert('Please enter a password.');
-
-    
 }
 
 $("#submit").click(function() {
-    var title = document.getElementById("event-title").value
-    var description = document.getElementById("description").value
-    var date = new Date(document.getElementById("date").value)
-    console.log(title)
-    console.log(db.Timestamp)
-    console.log(date)
+    var title = document.getElementById("event-title").value;
+    var description = document.getElementById("description").value;
+    var date = new Date(document.getElementById("date").value);
+    console.log(title);
+    console.log(db.Timestamp);
+    console.log(date);
     if (title !== "") {
-        db.collection("events").add({
+        var name = (title + " - " + date.toLocaleDateString('en-US')).replace(/\//g, "|");
+        db.collection("events").doc(name).set({
             "title": title,
             "description": description,
             "date": date.getTime() / 1000
-        })
+        }, {merge: true})
         .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id)
-            alert("Event created")
+            console.log("Document written with ID: ", docRef.id);
+            alert("Event created");
         })
         .catch(function(error) {
-            console.error("Error adding document: ", error)
-        })
+            console.error("Error adding document: ", error);
+        });
     }
-})
-
-
-// form.onsubmit = function() {
-    
-// };
+});
