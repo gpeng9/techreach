24879db799ee003e054992ce39e9a91c8bdc02bd
jshi3 <jshi3@caltech.edu>
2020-03-27 23:15:36 -0700

    Add documentation

--- user_app/public/index.js
+++ user_app/public/index.js
@@ -1,5 +1,6 @@
 (function(){
     "use strict";
+    // Initialize database
     var db = firebase.firestore();
     var eventsRef = db.collection("events");
     var form = document.getElementById("form");
@@ -9,7 +10,10 @@
     function standardDate(date) {
         return date.toLocaleDateString('en-US');
     }
+    // Adds submitted information to the database
     form.onsubmit = function() {
+        // Need to replace all forward slashes in the event name because they
+        // would be interpreted as database path delimiters
         var eventName = jumpMenu.value.replace(/\//g, "|");
         if (eventName !== "") {
             var eventRef = eventsRef.doc(eventName).collection("attendees");
@@ -21,6 +25,8 @@
             });
         }
     };
+    // Pulls the events list from the database and displays the ones that have
+    // not happened yet in the dropdown
     eventsRef.get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
             var data = doc.data();
@@ -29,6 +35,7 @@
             if (Date.now()/1000 < data.date) {
                 var option = document.createElement("option");
                 var date = new Date(data.date * 1000);
+                // Event names include both event title and date
                 option.text = data.title + " - " + standardDate(date);
                 jumpMenu.options.add(option);
             }
